1dde2d1af60cec882a3600a52a1a87110e3f509c
Henry Perkins <htperkins@gmail.com>
2026-06-05 19:47:09 +0000

    feat(home): register pattern block styles

--- wp-content/themes/henrys-digital-canvas/functions.php
+++ wp-content/themes/henrys-digital-canvas/functions.php
@@ -73,6 +73,13 @@ function hdc_enqueue_frontend_styles() {
 		hdc_asset_version( '/assets/css/home-sections.css' )
 	);
 
+	wp_enqueue_style(
+		'hdc-home-patterns',
+		get_stylesheet_directory_uri() . '/assets/css/home-patterns.css',
+		array( 'hdc-home-sections' ),
+		hdc_asset_version( '/assets/css/home-patterns.css' )
+	);
+
 	// Load single post styles on single posts
 	if ( is_single() ) {
 		wp_enqueue_style(
